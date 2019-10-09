08e74bb7492d5944eb00671e07af63a9498cf299
krzyczak <krzyczak@users.noreply.github.com>
2019-10-09 13:24:16 +0100

    Fixed spec/lib/graphene/graph/builder_spec.rb

--- spec/support/jobs/do_nothing.rb
+++ spec/support/jobs/do_nothing.rb
@@ -7,5 +7,29 @@ module Support
         # Do nothing
       end
     end
+
+    module QA
+      class IntegrityFilter < Support::Jobs::DoNothing; end;
+      class DuplicateFilter < Support::Jobs::DoNothing; end;
+      class DurationFilter < Support::Jobs::DoNothing; end;
+      class AdultContentFilter < Support::Jobs::DoNothing; end;
+    end
+
+    module Transform
+      class Zencoder < Support::Jobs::DoNothing; end;
+    end
+
+    module Process
+      class ExtractFrames < Support::Jobs::DoNothing; end;
+      class ExtractMetadata < Support::Jobs::DoNothing; end;
+      class AudioActivityDetection < Support::Jobs::DoNothing; end;
+      class VideoActivityDetection < Support::Jobs::DoNothing; end;
+      class GenerateThumbnails < Support::Jobs::DoNothing; end;
+    end
+
+    module Analysis
+      class BehaviouralRecognition < Support::Jobs::DoNothing; end;
+      class PeopleDetection < Support::Jobs::DoNothing; end;
+    end
   end
 end
