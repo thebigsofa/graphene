6f2ba297b275b9ffe951506a04ab90808ba07587
krzyczak <krzyczak@users.noreply.github.com>
2019-10-10 17:26:39 +0100

    Refactored status page

--- app/controllers/graphene/service_status_controller.rb
+++ app/controllers/graphene/service_status_controller.rb
@@ -1,30 +1,11 @@
 # frozen_string_literal: true
+require_dependency "graphene/application_controller"
 
 module Graphene
-  class ServiceStatusController < ApplicationController
-    # rubocop:disable Metrics/MethodLength
-    def status
-      body = <<~HTML.html_safe
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <title>Status</title>
-          </head>
-          <body>
-            <h2>[#{Time.now}] Chimera is Alive!</h2>
-            <h2>Chimera Options</h2>
-            <ul>
-              <li>
-                <a href="#{sidekiq_web_path}">Sidekiq</a>
-                <a href="#{ui_pipelines_path}">Pipeline UI</a>
-              </li>
-            </ul>
-          </body>
-        </html>
-      HTML
+  class ServiceStatusController < ActionController::Base
+    # layout "graphene/application"
 
-      render(html: body)
+    def show
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
