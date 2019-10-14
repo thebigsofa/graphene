a714e305a9a3167b21bd6063bece1fd35f9b9884
krzyczak <krzyczak@users.noreply.github.com>
2019-10-14 14:46:34 +0100

    Refactored Graphene configuration.

--- lib/graphene.rb
+++ lib/graphene.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "graphene/engine"
+require "sidekiq"
+require "pg_search"
+require "activerecord_json_validator"
 
 class NoAuthentication
   def initialize(app)
@@ -12,6 +15,15 @@ class NoAuthentication
   end
 end
 
+class JobsTrackingDisabled
+  include Sidekiq::Worker
+
+  def perform(_)
+    ::Sidekiq.logger.info("Sidekiq tracking is disabled.")
+  end
+end
+
+
 module Graphene
   class << self
     attr_accessor :config
@@ -20,11 +32,13 @@ module Graphene
   def self.configure
     self.config ||= Config.new
     yield(config)
-    self.config.sidekiq_tracker ||= Graphene::Tracking::Disabled
+    self.config.sidekiq_tracker ||= JobsTrackingDisabled
     self.config.auth_middleware ||= NoAuthentication
   end
 
   class Config
     attr_accessor :sidekiq_tracker, :auth_middleware
   end
+
+  self.configure {}
 end
