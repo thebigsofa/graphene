6e569bff573adf89dd2fd40002f49129b091fc98
krzyczak <krzyczak@users.noreply.github.com>
2019-10-17 15:59:24 +0100

    Updated test/dummy/db/schema.rb to the newest version.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_132501) do
+ActiveRecord::Schema.define(version: 2019_10_11_111729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2019_06_06_132501) do
     t.text "content"
     t.uuid "searchable_id"
     t.string "searchable_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["searchable_id", "searchable_type"], name: "index_pg_search_documents_on_searchable_id_and_searchable_type"
     t.index ["searchable_id"], name: "index_pg_search_documents_on_searchable_id"
   end
